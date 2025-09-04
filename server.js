758e1200b8e2ee7f0362d7485323b5b48be5f201
hilamatu <hil21006@byui.edu>
2025-09-04 19:11:36 +0900

    Step 4 completed

--- server.js
+++ server.js
@@ -5,10 +5,21 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("espress-ejs-layouts") 
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") //declare that ejs will be the view engine for our application
+app.use(expressLayouts) //tells the application to use the express-ejs-layouts package
+app.set("layout", "./layouts/layout") // not at views root when the express ejs layout goes 
+/************************************** looking for the basic template for a view, it will find it in a layouts folder, and the template will be named layout.
+
 
 /* ***********************
  * Routes
